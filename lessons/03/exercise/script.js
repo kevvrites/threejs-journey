9e4c11d2b0d27f89f49919e06a9a53b9a5463260
Kevin Liu <liukproductivity@gmail.com>
2024-04-25 20:04:27 -0400

    completed lesson 3, started lesson 4

---
+++ lessons/03/exercise/script.js
@@ -0,0 +1,27 @@
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas.webgl");
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
